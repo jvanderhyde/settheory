2802535363f005e3d5c95e719d5762a107cdcd89
jamesvhyde <jamesvhyde@hotmail.com>
2012-01-23 14:04:54 -0600

    Added a main file

---
+++ src/edu/benedictine/settheory/SetTheory.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.benedictine.settheory;
+
+/**
+ *
+ * @author jamesvanderhyde
+ */
+public class SetTheory
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
